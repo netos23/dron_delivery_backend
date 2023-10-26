3b83731269a322e54d7c2eeac56705dab10e2a65
iushishko.adm <iushishko@sitronics.com>
2023-10-26 17:09:33 +0300

    admin for satellite

--- core/satellites/admin.py
+++ core/satellites/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
 
 from satellites.models import SatelliteModel
 
 # Register your models here.
-admin.site.register(SatelliteModel)
+@admin.register(SatelliteModel)
+class SatelliteModel(admin.ModelAdmin):
+    list_display = ('object_id', "picture_image", "object_name", "is_active")
+
+    def picture_image(self, obj):
+        return mark_safe(f'<img src="{obj.picture or ""}" width="150" height="150" /> ')
