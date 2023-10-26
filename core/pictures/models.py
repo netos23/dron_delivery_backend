779afe70d8db1498aa8b9ed210ec8ac009397ff2
iushishko.adm <iushishko@sitronics.com>
2023-10-26 23:16:23 +0300

    auth fix

--- core/pictures/models.py
+++ core/pictures/models.py
@@ -14,9 +14,10 @@ class SatellitePictureModel(models.Model):
     def __str__(self):
         return f'{str(self.link)}'
 
+
 class PictureModel(models.Model):
-    url = models.ImageField(max_length=256, upload_to='pictures/', null=True)
-    link = models.CharField(max_length=256, null=True)
+    url = models.ImageField(max_length=256, upload_to='pictures/', null=True, blank=True)
+    link = models.CharField(max_length=256, null=True, blank=True)
 
     def __str__(self):
         return f'{str(self.url)}'
