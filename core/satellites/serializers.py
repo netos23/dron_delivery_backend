f4f68f56782aeab046e2122e932cd01d9968739a
iushishko.adm <iushishko@sitronics.com>
2023-10-27 15:36:53 +0300

    tarifs

--- core/satellites/serializers.py
+++ core/satellites/serializers.py
@@ -3,7 +3,7 @@ from pictures.serializers import PictureSerializer
 
 
 class SatteliteSerializer(serializers.Serializer):
-    id = serializers.CharField(source='object_id')
+    id = serializers.CharField()
     name = serializers.CharField(source='object_name')
     picture = serializers.URLField()
     resolution = serializers.FloatField()
