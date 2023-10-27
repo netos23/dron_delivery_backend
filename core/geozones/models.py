6742749475ba98db54e38c7870a940126fa75c42
git-gud-casual <popasena2@ya.ru>
2023-10-27 15:41:56 +0300

    Added tests

--- core/geozones/models.py
+++ core/geozones/models.py
@@ -1,27 +1,27 @@
 from django.db import models
-from django.contrib.gis.geos import GEOSGeometry, Point, MultiPolygon
+from django.contrib.gis.geos import GEOSGeometry, Point, MultiPolygon, WKTReader
 from django.contrib.gis.db import models
 from authorization.models import Users
 
 
 class GeozoneModel(models.Model):
-    geom = models.MultiPolygonField(srid=4326)
+    geom = models.MultiPolygonField(srid=4326, null=True)
     wkt = models.TextField()
     name = models.CharField(max_length=50)
     user = models.ForeignKey(Users, on_delete=models.SET_NULL, null=True, blank=True)
 
     def save(self, *args, **kwargs):
         if getattr(self, 'wkt'):
-            self.geom = MultiPolygon(self.wkt)
+            self.geom = MultiPolygon(WKTReader().read(self.wkt))
         super(GeozoneModel, self).save(*args, **kwargs)
 
 
 class DeprecatedGeozoneModel(models.Model):
-    geom = models.MultiPolygonField(srid=4326)
+    geom = models.MultiPolygonField(srid=4326, null=True)
     wkt = models.TextField()
     name = models.CharField(max_length=50)
 
     def save(self, *args, **kwargs):
         if getattr(self, 'wkt'):
-            self.geom = MultiPolygon(self.wkt)
+            self.geom = MultiPolygon(WKTReader().read(self.wkt))
         super(DeprecatedGeozoneModel, self).save(*args, **kwargs)
--- core/geozones/tests.py
+++ core/geozones/tests.py
@@ -1,3 +1,36 @@
 from django.test import TestCase
 
-# Create your tests here.
+from geozones.models import GeozoneModel, DeprecatedGeozoneModel
+from django.contrib.gis.geos.point import Point
+
+
+class GeozoneTest(TestCase):
+    geozone: GeozoneModel
+    depr_geo: DeprecatedGeozoneModel
+    point1 = Point(0.75, 0.75)
+    point2 = Point(2, 2)
+
+    def setUp(self):
+        geozone = GeozoneModel()
+        geozone.wkt = "POLYGON ((0 0, 1.5 0, 1.5 1, 0 1.5, 0 0))"
+        geozone.name = "test"
+        geozone.save()
+        self.geozone = geozone
+
+        depr_geo = DeprecatedGeozoneModel()
+        depr_geo.wkt = "MULTIPOLYGON (((30 20, 45 40, 10 40, 30 20)), ((15 5, 40 10, 10 20, 5 10, 15 5)))"
+        depr_geo.name = "test"
+        depr_geo.save()
+        self.depr_geo = depr_geo
+
+    def test_field(self):
+        geo = self.geozone
+        self.assertTrue(geo.geom.contains(self.point1))
+        self.assertFalse(geo.geom.contains(self.point2))
+
+    def test_query(self):
+        geo_original = self.geozone
+        geo1 = GeozoneModel.objects.filter(geom__contains=self.point1).first()
+        self.assertEqual(geo_original, geo1)
+        geo2 = GeozoneModel.objects.exclude(geom__contains=self.point2).first()
+        self.assertEqual(geo_original, geo2)
