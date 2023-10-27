9bb4b1aefbdaf3f51b48e955a89f42c5244a35d8
iushishko.adm <iushishko@sitronics.com>
2023-10-27 10:49:24 +0300

    deprecated geozones

--- core/geozones/admin.py
+++ core/geozones/admin.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
 from geozones.models import GeozoneModel, DeprecatedGeozoneModel
 
-admin.site.register(DeprecatedGeozoneModel)
 
 
 @admin.register(GeozoneModel)
 class GeozoneAdmin(admin.ModelAdmin):
+    list_display = ('id', "name", "wkt")
+
+@admin.register(DeprecatedGeozoneModel)
+class DeprecatedGeozoneModelAdmin(admin.ModelAdmin):
     list_display = ('id', "name", "wkt")
