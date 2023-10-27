2dd088c3f1232ec7995da5de7c99e2987d2cb5b2
iushishko.adm <iushishko@sitronics.com>
2023-10-27 23:37:36 +0300

    satellite serializer

--- core/satellites/serializers.py
+++ core/satellites/serializers.py
@@ -4,14 +4,14 @@ from satellites.models import PositionModel
 
 
 class SatteliteSerializer(serializers.Serializer):
-    id = serializers.CharField()
+    id = serializers.IntegerField()
     name = serializers.CharField(source='object_name')
     picture = serializers.URLField()
     resolution = serializers.FloatField()
 
 
 class RequestPositionSerializer(serializers.Serializer):
-    satellite_id = serializers.CharField(required=True)
+    satellite_id = serializers.IntegerField(required=True)
 
 
 class PositionSerializer(serializers.ModelSerializer):
