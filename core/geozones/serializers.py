07bdf8a376da06ae51a74ebf3cd72c64ab38aff9
git-gud-casual <popasena2@ya.ru>
2023-10-27 14:09:47 +0300

    Added serializers for geozones

--- core/geozones/serializers.py
+++ core/geozones/serializers.py
@@ -1,10 +1,76 @@
+from typing import List
+
 from rest_framework import serializers
+from geozones.models import GeozoneModel, DeprecatedGeozoneModel
 
 
-class GeozoneSerializer(serializers.Serializer):
-    id = serializers.CharField()
+def tuple_validation(value):
+    print(value)
+    if len(value) != 2:
+        raise serializers.ValidationError("Waited 2 floats")
+
+
+def wkt_validation(value):
+    if len(value) <= 0:
+        raise serializers.ValidationError("Waited Polygons")
+
+
+def polygon_validation(value):
+    if len(value) < 3:
+        raise serializers.ValidationError("Expected 3 or more points")
+
+
+class WktField(serializers.ListField):
+    default_validators = [wkt_validation]
+    child = serializers.ListField(
+                                    child=serializers.ListField(child=serializers.FloatField(),
+                                                                validators=[tuple_validation]),
+                                    validators=[polygon_validation]
+                                )
+
+    @staticmethod
+    def _polygon_to_wkt(polygon: List[List[float]]) -> str:
+        return "(" + ", ".join(map(lambda points: f"{points[0]} {points[1]}", polygon)) + ")"
+
+    @staticmethod
+    def _wkt_polygon_to_list(wkt: str) -> List[List[float]]:
+        wkt = wkt.rstrip(")").lstrip("(")
+        return list(map(lambda x: list(map(float, x.split(" "))), wkt.split(", ")))
+
+    def to_representation(self, data: str):
+        polygons = []
+        data = data.lstrip("MULTIPOLYGON ").lstrip("POLYGON ").lstrip("(").rstrip(")")
+        for polygon_s in data.split("), "):
+            polygon_s = polygon_s.lstrip("(")
+            polygons.append(self._wkt_polygon_to_list(polygon_s))
+        return polygons
+
+    def to_internal_value(self, data):
+        if len(data) > 1:
+            s = "MULTIPOLYGON ("
+            for polygon in data:
+                s += "(" + self._polygon_to_wkt(polygon) + "), "
+            return s.rstrip(", ") + ")"
+        else:
+            return "POLYGON (" + self._polygon_to_wkt(data[0]) + ")"
+
+
+class DeprecatedGeozoneSerializer(serializers.Serializer):
+    id = serializers.IntegerField(required=False)
     name = serializers.CharField()
-    wkt = serializers.CharField()
+    wkt = WktField()
+
+    def create(self, validated_data):
+        del validated_data["id"]
+        return DeprecatedGeozoneModel.objects.create(**validated_data)
+
+
+class GeozoneSerializer(DeprecatedGeozoneSerializer):
+    user = serializers.IntegerField(required=False)
+
+    def create(self, validated_data):
+        del validated_data["id"]
+        return GeozoneModel.objects.create(**validated_data)
 
 
 class RequestGeozoneSerializer(serializers.Serializer):
--- core/geozones/views.py
+++ core/geozones/views.py
@@ -1,14 +1,15 @@
 from django.db.models import Q
 from drf_yasg.utils import swagger_auto_schema
-from rest_framework import generics, status
+from rest_framework import generics, status, mixins
 from rest_framework.renderers import JSONRenderer
 from rest_framework.response import Response
 
-from geozones.models import GeozoneModel
-from geozones.serializers import GeozoneSerializer, RequestGeozoneSerializer
+from geozones.models import GeozoneModel, DeprecatedGeozoneModel
+from geozones.serializers import GeozoneSerializer, RequestGeozoneSerializer, DeprecatedGeozoneSerializer
 
 
-class GeozoneView(generics.GenericAPIView):
+class GeozoneView(generics.GenericAPIView,
+                  mixins.CreateModelMixin):
     """Show all geozones."""
 
     request_serializer = RequestGeozoneSerializer
@@ -27,15 +28,27 @@ class GeozoneView(generics.GenericAPIView):
             geozones = GeozoneModel.objects.filter(Q(user_id__isnull=show_public))
         return Response(data=self.serializer_class(geozones, many=True).data, status=status.HTTP_200_OK)
 
+    @swagger_auto_schema(query_serializer=serializer_class(), responses={"201": serializer_class()})
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
 
-class DeprecatedGeozoneView(generics.GenericAPIView):
+
+class DeprecatedGeozoneView(generics.GenericAPIView,
+                            mixins.CreateModelMixin):
     """Show all geozones."""
 
     request_serializer = RequestGeozoneSerializer
-    serializer_class = GeozoneSerializer
+    serializer_class = DeprecatedGeozoneSerializer
     renderer_classes = [JSONRenderer]
 
     @swagger_auto_schema(responses={200: serializer_class()})
     def get(self, request):
-        return Response(data=self.serializer_class(GeozoneModel.objects.all(), many=True).data,
+        return Response(data=self.serializer_class(DeprecatedGeozoneModel.objects.all(), many=True).data,
                         status=status.HTTP_200_OK)
+
+    @swagger_auto_schema(request_body=serializer_class(), responses={"201": serializer_class()})
+    def post(self, request, *args, **kwargs):
+        serializer = DeprecatedGeozoneSerializer(data=request.body)
+        serializer.is_valid(raise_exception=True)
+        return self.create(request, *args, **kwargs)
+
