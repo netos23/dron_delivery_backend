6c230daa30ed8dff2671173bea4d83b5523ff848
iushishko.adm <iushishko@sitronics.com>
2023-10-28 10:58:49 +0300

    dashdoar

--- core/dashboard/views.py
+++ core/dashboard/views.py
@@ -3,12 +3,14 @@ from django.shortcuts import render
 # from core.order.models import OrderModel
 from django.core import serializers
 
+from orders.models import OrderModel
+
 
 def dashboard_with_pivot(request):
     return render(request, 'dashbord.html', {})
 
 
 def pivot_data(request):
-    # dataset = OrderModel.objects.all()
-    # data = serializers.serialize('json', dataset)
-    return JsonResponse([], safe=False)
+    dataset = OrderModel.objects.all()
+    data = serializers.serialize('json', dataset)
+    return JsonResponse(data, safe=False)
